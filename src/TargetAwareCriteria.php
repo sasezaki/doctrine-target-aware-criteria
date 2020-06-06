d61863d5053664a9ffdbde053a62eb8fd4ae4f1f
Toshiyuki FUJITA <kalibora@gmail.com>
2020-06-06 17:06:03 +0900

    Redefine create

--- src/TargetAwareCriteria.php
+++ src/TargetAwareCriteria.php
@@ -6,4 +6,15 @@ use Doctrine\Common\Collections\Criteria;
 
 abstract class TargetAwareCriteria extends Criteria implements TargetAwareCriteriaInterface
 {
+    /**
+     * Creates an instance of the class.
+     *
+     * (Redefine the type hints to make them correct)
+     *
+     * @return static
+     */
+    public static function create() : self
+    {
+        return new static();
+    }
 }
